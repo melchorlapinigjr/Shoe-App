95b118d73881b1d5a176ee7ce9c4fbed74284503
akosijho <jhobertpanerio@gmail.com>
2022-08-07 03:09:13 +0800

    Implement Api communication. Some adjustments.

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -4,4 +4,4 @@ const kTextColor = Colors.white;
 const kTextLightColor = Colors.black;
 
 const kDefaultPaddin = 8.0;
-const baseUrl = "http://192.168.43.183:8080/api";
+const baseUrl = "http://192.168.137.1:8080/api";
