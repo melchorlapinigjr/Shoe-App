1c6c37af3577494a842158c5af77711322091d1f
akosijho <jhobertpanerio@gmail.com>
2022-08-11 04:08:52 +0800

    Implement Api communication. Implement Google SignIn and put signIn data to profile page. Implement auto login by SharedPreference Some adjustments.

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_shoe_app/app/app.locator.dart';
 import 'package:flutter_shoe_app/app/app.router.dart';
+import 'package:flutter_shoe_app/core/services/shared_preferrence/shared_preference.dart';
 import 'package:flutter_shoe_app/views/application/application_view_model.dart';
+import 'package:flutter_shoe_app/views/home/homepage_view.dart';
 import 'package:flutter_shoe_app/views/login/log_in_view.dart';
+import 'package:get/get.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:stacked/stacked.dart';
-import 'package:get/get.dart';
 
 import 'core/services/navigation/navigation_service.dart';
 
@@ -24,6 +26,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final SharedPreference sharedPreference = locator<SharedPreference>();
+    var user = sharedPreference.getUser();
     return GetMaterialApp(
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -37,7 +41,7 @@ class MyApp extends StatelessWidget {
       home: ViewModelBuilder<ApplicationViewModel>.reactive(
         builder: (context, model, child) {
           //return const HomepageView();
-          return const LoginView();
+          return user == null ? const LoginView() : const HomepageView();
         },
         viewModelBuilder: () => ApplicationViewModel(),
         disposeViewModel: false,
