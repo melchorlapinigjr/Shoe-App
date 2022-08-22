f7a513fd6a437fa909c3d627aa30abce6b01cd98
akosijho <jhobertpanerio@gmail.com>
2022-08-22 14:57:25 +0800

    Display cart page, add and remove cart page

--- lib/views/cart/cart_page_view.dart
+++ lib/views/cart/cart_page_view.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_shoe_app/views/application/application_view_model.dart';
 import 'package:flutter_shoe_app/views/cart/cart_item_view.dart';
-import 'package:stacked/stacked.dart';
 import 'package:provider/provider.dart';
+import 'package:stacked/stacked.dart';
 
 class CartPageView extends StatelessWidget {
   const CartPageView({Key? key}) : super(key: key);
@@ -12,60 +12,67 @@ class CartPageView extends StatelessWidget {
     final applicationViewModel = Provider.of<ApplicationViewModel>(context);
     return ViewModelBuilder<ApplicationViewModel>.reactive(
         disposeViewModel: false,
+        onModelReady: (model) => model.getMyCart(),
         viewModelBuilder: () => applicationViewModel,
         builder: (context, viewModel, child) {
-          return viewModel.cart.isNotEmpty ? Container(
-            padding: const EdgeInsets.fromLTRB(16, 0, 16, 8),
-            child: SingleChildScrollView(
-              scrollDirection: Axis.vertical,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                //crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  ListView.builder(
-                    primary: false,
-                    shrinkWrap: true,
-                    itemCount: applicationViewModel.cart.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      final shoe =
-                          applicationViewModel.cart.keys.toList()[index];
-                      final quantity = applicationViewModel.cart[shoe] ?? 0;
-                      return CartItemView(
-                        shoe: shoe,
-                        quantity: quantity,
-                      );
-                    },
-                  ),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  SizedBox(
-                    width: double.infinity,
-                    height: 48,
-                    child: ElevatedButton(
-                      style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all(
-                              const Color(0xff1F2732)),
-                          shape:
-                              MaterialStateProperty.all<RoundedRectangleBorder>(
-                                  RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(12.0),
-                          ))),
-                      onPressed: () {},
-                      child: const Text('Checkout',
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w800,
+          return viewModel.cart.isNotEmpty
+              ? Container(
+                  padding: const EdgeInsets.fromLTRB(16, 0, 16, 8),
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.vertical,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      //crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        ListView.builder(
+                          primary: false,
+                          shrinkWrap: true,
+                          itemCount: applicationViewModel.cart.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            final shoe =
+                                applicationViewModel.cart.keys.toList()[index];
+                            final cartObject =
+                                applicationViewModel.tempCart[index];
+                            final quantity =
+                                applicationViewModel.cart[shoe] ?? 0;
+                            return CartItemView(
+                              shoe: shoe,
+                              quantity: quantity,
+                              cartObject: cartObject,
+                            );
+                          },
+                        ),
+                        const SizedBox(
+                          height: 16,
+                        ),
+                        SizedBox(
+                          width: double.infinity,
+                          height: 48,
+                          child: ElevatedButton(
+                            style: ButtonStyle(
+                                backgroundColor: MaterialStateProperty.all(
+                                    const Color(0xff1F2732)),
+                                shape: MaterialStateProperty.all<
+                                        RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(12.0),
+                                ))),
+                            onPressed: () {},
+                            child: const Text('Checkout',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w800,
+                                ),
+                                textAlign: TextAlign.center),
                           ),
-                          textAlign: TextAlign.center),
+                        )
+                      ],
                     ),
-                  )
-                ],
-              ),
-            ),
-          ) : const Center(
-            child: Text('You have no items in cart'),
-          );
+                  ),
+                )
+              : const Center(
+                  child: Text('You have no items in cart'),
+                );
         });
   }
 }
